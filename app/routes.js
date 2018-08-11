17d5ff8599e8ab4a00998733363cbd3e6b71f52d
Shishir yadav <shishir569@gmail.com>
2018-08-11 23:33:51 +0530

    Update routes.js

--- app/routes.js
+++ app/routes.js
@@ -1,17 +1,54 @@
+var request = require('request');
+
 module.exports = function (app, streams) {
 
-  // GET home 
-  var index = function (req, res) {
-    res.render('index', {
-      title: 'Project RTC',
-      header: 'WebRTC live streaming',
-      username: 'Username',
-      share: 'Share this link',
-      footer: 'Shishir569@gmail.com',
-      id: req.params.id
-    });
-  };
+// GET home 
 
+var index = function (req, res) {
+
+console.log(req.query.userId);
+
+var userId = req.query.userId;
+
+var options = {
+
+method: 'POST',
+
+url: 'http://23.101.141.185:3000/letsgolive/api/v1/liveChanel.php <http://23.101.141.185:3000/letsgolive/api/v1/liveChanel.php> ',
+
+body: '{"user_id":'+userId+',"chanel":"chanel"}'
+
+};
+
+ 
+
+request(options, function (error, response, body) {
+
+if (error) throw new Error(error);
+
+ 
+
+console.log(body);
+
+});
+
+res.render('index', {
+
+title: 'Project RTC',
+
+header: 'WebRTC live streaming',
+
+username: 'Username',
+
+share: 'Share this link',
+
+footer: 'Shishir569@gmail.com',
+
+id: req.params.id
+
+});
+
+};
   var index1 = function (req, res) {
     res.render('index1', {
       title: 'Project RTC',
