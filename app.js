28cd4ff7898c54cde6357975a0f38921713aa1bc
Shishir yadav <shishir569@gmail.com>
2018-08-12 01:56:38 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -2,16 +2,14 @@
  * Module dependencies.
  */
 var express = require('express')
-  , path = require('path')
-  , fs = require('fs')
-  , https = require('https')
-  , streams = require('./app/streams.js')();
+,	path = require('path')
+,	streams = require('./app/streams.js')();
 
 var favicon = require('serve-favicon')
-  , logger = require('morgan')
-  , methodOverride = require('method-override')
-  , bodyParser = require('body-parser')
-  , errorHandler = require('errorhandler');
+,	logger = require('morgan')
+,	methodOverride = require('method-override')
+,	bodyParser = require('body-parser')
+,	errorHandler = require('errorhandler');
 
 var app = express();
 
@@ -26,20 +24,19 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(methodOverride());
 app.use(express.static(path.join(__dirname, 'public')));
 
-//var port = app.get('port');
 // development only
 if ('development' == app.get('env')) {
   app.use(errorHandler());
 }
 
 // routing
-require('./app/routes.js')(app, streams);
+require('./app/routes.js')(app, streams);
+
 var server = app.listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port')); 
+  console.log('Express server listening on port ' + app.get('port'));
 });
 
 var io = require('socket.io').listen(server);
-//var io = require('socket.io').listen(server);
 /**
  * Socket.io event handling
  */
